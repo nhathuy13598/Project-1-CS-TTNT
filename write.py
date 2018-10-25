2560da739be7297aff7a363b5dec44b5bdc7923f
NhatHuyTran <nhathuy13598@gmail.com>
2018-10-25 21:39:27 +0700

    Sua lai loi khi in ket qua ra file

--- write.py
+++ write.py
@@ -4,25 +4,30 @@ from Class import ClassNode
 def writeFile(filename,mode,data):
 	try:
 		with open(filename, mode) as file:
+			# In do dai duong di
 			file.write(int.__str__(data[0])+'\n')
+
+			# In duong di
 			for i in data[2]:
 				file.write("({0},{1}) ".format(i.x,i.y))
 			file.write("\n")
-			for i in range(len(data[1])):
+
+			# In ma tran duong di
+			size = len(data[1])
+			for i in range(size):
 				str = ""
-				for j in range(len(data[1])):
+				for j in range(size):
 					
-					if (i == data[3].x) & (j == data[3].y):
+					if (i == data[3].x) and (j == data[3].y):
 						str = str + "S "
+					elif i == data[4].x and j == data[4].y:
+						str = str + "G "
 					elif data[2].is_containt(ClassNode.Node(i,j,None)):
 						str = str + "x "
-					elif i == data[4].x & j == data[4].y:
-						str = str + "G "
+					elif data[1][i][j] == 0:
+						str = str + "- "
 					else:
-						if data[1][i][j] == 0:
-							str = str + "- "
-						else:
-							str = str + "o "
+						str = str + "o "
 				file.write(str+'\n')
 			return True
 	except EOFError:
